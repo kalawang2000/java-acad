183a57a1568717d1761d935403ca36a3b0b3b6fc
kalawang2000 <russell.limcangco@gmail.com>
2016-01-25 13:03:24 +0800

    first commit

---
+++ Ass5.java
@@ -0,0 +1,21 @@
+
+class Ass5 {
+    public static void main(String[] args) {
+        int x=5, y=10;
+		int suma, sumb,sumc;
+		float sumd;
+		
+		suma = x + (y*2);
+		sumb = x - y + 2;
+		sumc = (x+y) * 2;
+		sumd = (float)y/100 * x;	
+		
+		 System.out.println("Sum of x + (y*2) = " + suma);
+		 System.out.println("Sum of x - y + 2 = " + sumb);
+		 System.out.println("Sum of (x+y) * 2 = " + sumc);
+		 System.out.println("Sum of y % x = " + sumd);
+		 
+		
+		
+    }
+}
---
+++ Sumtwo.java
@@ -0,0 +1,14 @@
+
+class Sumtwo {
+    public static void main(String[] args) {
+        int first=5;
+		float second = 5.5f;
+		int sum;
+		
+		
+		sum = first + (int)second;
+		
+		 System.out.println("Sum of numbers is " + sum);
+		
+    }
+}
