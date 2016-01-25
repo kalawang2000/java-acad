91be8aed12b956fce13b3dc3a5a57f0285a2dffe
kalawang2000 <russell.limcangco@gmail.com>
2016-01-26 03:02:27 +0800

    upload

---
+++ prime.java
@@ -0,0 +1,24 @@
+class prime {
+    public static void main(String[] args) {
+		int n=100;
+		boolean  pr;
+		for (int i=1;i<=n; i++)
+		{
+			pr=false;
+		
+			for (int j=2;j<i; j++)
+			{
+				
+				if (i%j==0) 
+				{
+					pr=true;
+					break;
+				}			  
+			   
+			}
+			if (pr==false)
+			System.out.print(i+ " ");
+		}
+		
+    }
+}
